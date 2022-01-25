dfa4913cd3a583b1d460b9cc99b76c522d0f87f5
Sammarth Kumar <kumarsammarth@gmail.com>
2022-01-25 22:45:30 +0530

    updates

--- email_send.py
+++ email_send.py
@@ -1,38 +1,36 @@
 from tkinter import *
 import mysql.connector
+import execution
 
-root=Tk()
+root = Tk()
 
-db=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="", 
-    database="email_bot"
-)
+db = execution.db_connect()
 
-mycursor=db.cursor()
+mycursor = db.cursor()
 
-contact_mang_title=Label(root,text="Manage contacts")
-contact_mang_title.grid(row=0,column=0)
 
+contact_mang_title = Label(root, text="Manage contacts")
+contact_mang_title.grid(row=0, column=0)
+
+# select contacts from list
 mycursor.execute("select * from contacts;")
-names=[0]
-emails=[0]
+names = [0]
+emails = [0]
 for i in mycursor:
-    names.append(i[0]+" "+i[1])
-    emails.append(i[2])                                                                     
+    names.append(i[0] + " " + i[1])
+    emails.append(i[2])
 
 print(names)
 print(emails)
 
 scrollbar_showitems = Scrollbar(root)
-scrollbar_showitems.grid(row=1,column=1,ipady=60)
-listbox_emails=Listbox(root, selectmode="multiple",selectbackground="green")
-for i in range(1,len(emails)):
-    listbox_emails.insert(i,emails[i])
+scrollbar_showitems.grid(row=1, column=1, ipady=60)
+listbox_emails = Listbox(root, selectmode="multiple", selectbackground="green")
+for i in range(1, len(emails)):
+    listbox_emails.insert(i, emails[i])
 
 
-listbox_emails.grid(row=1,column=0,ipadx=60)
+listbox_emails.grid(row=1, column=0, ipadx=60)
 listbox_emails.config(yscrollcommand=scrollbar_showitems.set)
 scrollbar_showitems.config(command=listbox_emails.yview)
 
--- execution.py
+++ execution.py
@@ -1,49 +1,23 @@
 import mysql.connector
 from tkinter import *
 
-db=mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="arjun2004",
-    database="email_bot"
-)
 
-mycursor=db.cursor(buffered=True)
+def db_connect():
 
-root=Tk()
+    try:
+        db = mysql.connector.connect(
+            host="localhost", user="root", passwd="password", database="email_bot"
+        )
 
-def removeall():
-    title.grid_forget()
-    username_label.grid_forget()
-    username_entry.grid_forget()
-    password_label.grid_forget()
-    password_entry.grid_forget()
+    # create database if it doesn't exist
+    except:
+        db = mysql.connector.connect(host="localhost", user="root", passwd="password")
+
+        db.cursor(buffered=True).execute("create database email_bot")
+
+    return db
 
 
+db = db_connect()
 
-#Login page
-def login():
-    #global title,username_label,username_entry,password_label,password_entry,login_button
-    mail=username_entry.get()
-    pwd=password_entry.get()
-    mycursor.execute("select email_id,password from email_list;")
-    for i in mycursor:
-        #print(i)
-        if i[0]==mail and i[1]==pwd:
-            name=mail[0:mail.index("@")]
-            print(name)
-            print("Logged in!")
-            #removeall()
-            break   
-    else:
-        print("Invalid info")
-
-global username_entry,username_label
-title=Label(root,text="Email Bot").grid(row=0,column=0)
-username_label=Label(root, text="Enter Email").grid(row=1, column=0)
-username_entry=Entry(root, exportselection=0, fg="blue").grid(row=1, column=1)
-password_label=Label(root, text="Enter Password").grid(row=2, column=0)
-password_entry=Entry(root, exportselection=0, fg="blue", show="●").grid(row=2, column=1)
-login_button=Button(root,text="Login",command=login).grid(row=3,column=0)
-
-root.mainloop()
+root = Tk()
